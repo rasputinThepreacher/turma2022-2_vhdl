e3d418eb0ea8f5a47a389e4b082cfa4d85f2e0db
fpfrimer <fpfirmer@gmail.com>
2024-11-08 17:35:37 -0300

    mux

--- codes/a15a_mux.vhd
+++ codes/a15a_mux.vhd
@@ -1,32 +1,36 @@
-library ieee;
-use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
+-- Este design VHDL implementa um multiplexador (mux) de 4 para 1 usando o comando 'case'.
+-- O 'case' é utilizado para selecionar entre múltiplas fontes de entrada com base no valor
+-- de um sinal de seleção ('sel'). Este código é otimizado para síntese, significando que pode
+-- ser convertido diretamente em lógica de hardware. O 'case' facilita a implementação de
+-- lógica condicional em designs de hardware, permitindo escolhas claras entre várias opções de sinal.
 
-entity a15_mux is
+library ieee;
+use ieee.std_logic_1164.all; -- Biblioteca padrão para o uso de sinais lógicos.
+use ieee.numeric_std.all; -- Biblioteca para operações aritméticas e lógicas com tipos 'signed' e 'unsigned'.
+
+entity a15a_mux is
     port(
-        sig0, sig1, sig2, sig3  :   in  unsigned(7 downto 0);
-        sel                     :   in  unsigned(1 downto 0);
-        saida                   :   out unsigned(7 downto 0));
+        sig0, sig1, sig2, sig3  :   in  unsigned(7 downto 0); -- Entradas do mux, cada uma de 8 bits.
+        sel                     :   in  unsigned(1 downto 0); -- Sinal de seleção de 2 bits para escolher entre as entradas.
+        saida                   :   out unsigned(7 downto 0)); -- Saída do mux de 8 bits.
 end entity;
 
-architecture rtl of a15_mux is
+architecture rtl of a15a_mux is
 begin
-
+    -- Processo sensível aos sinais de entrada e ao sinal de seleção.
     process(sel, sig0, sig1, sig2, sig3) is
     begin
         case sel is
             when "00" =>
-                saida <= sig0;
+                saida <= sig0; -- Se sel é "00", a saída é sig0.
             when "01" =>
-                saida <= sig1;
+                saida <= sig1; -- Se sel é "01", a saída é sig1.
             when "10" =>
-                saida <= sig2;
+                saida <= sig2; -- Se sel é "10", a saída é sig2.
             when "11" =>
-                saida <= sig3;
-            when others => -- 'U', 'X', '-', etc
-                saida <= (others => 'X');
+                saida <= sig3; -- Se sel é "11", a saída é sig3.
+            when others => -- Trata casos de valores indefinidos ou inesperados de 'sel'.
+                saida <= (others => 'X'); -- Atribui 'X' (desconhecido) a toda a saída.
         end case;
-
     end process;
-
-end rtl ; -- rtl
+end rtl; -- Fim da arquitetura RTL.
