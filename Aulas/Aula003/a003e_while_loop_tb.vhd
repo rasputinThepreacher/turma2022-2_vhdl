24fb02f698d4805470e1d3dea2b99c9a91c953e4
Yan Danilo Lima Morejon <yan.dl.morejon@gmail.com>
2025-04-14 09:06:59 -0300

    update

--- Aulas/Aula003/a003e_while_loop_tb.vhd
+++ Aulas/Aula003/a003e_while_loop_tb.vhd
@@ -7,20 +7,43 @@ entity a003e_while_loop_tb is
 end entity;
 
 architecture sim of a003e_while_loop_tb is
-    constant n  :   integer := 9; -- Declara a constante 'n' com o valor 9 para uso como condição de saída do loop.
+    --constant n  :   integer := 9; -- Declara a constante 'n' com o valor 9 para uso como condição de saída do loop.
+    constant n  :   integer := 100; -- Valor modificado por mim do código
+    constant FIB_LIMIT : integer := 100;   -- Valor modificado por mim do código 
+      
 begin
 
     -- Thread
     process is
-        variable i  :   integer := 20; -- Inicializa a variável 'i' em 20 para contar regressivamente até 1.
+        --variable i  :   integer := 20; -- Inicializa a variável 'i' em 20 para contar regressivamente até 1.
         -- Note que i foi declarado sem range, portanto, é uma variável de 32 bits
+        variable i  :   integer := 0;
+        variable num1 : integer := 0;       -- Valor modificado por mim do código 
+        variable num2 : integer := 1;       -- Valor modificado por mim do código 
+        variable a : integer := 0;
+        
     begin
         report "Start!";
+            
+            report "i = " & integer'image(num1);           -- Valor modificado por mim do código
+            report "i = " & integer'image(num2);           -- Valor modificado por mim do código
         
-        while i > 0 loop
-            exit when i = n;
-            report "i = " & integer'image(i);
-            i := i - 1;
+        while i < FIB_LIMIT loop
+
+            
+            --exit when i = n;
+
+            exit when a > FIB_LIMIT;
+
+            a := num1 + num2;    -- valor modificado por mim 
+            report "i = " & integer'image(a);
+
+            num1 := num2;
+            num2 := a;
+            
+            i := i + 1;       -- valor modificado por mim     PARA ITERAR
+            --i := i - 1;
+
         end loop;
 
         report "Ateh!";
