4867961d8f4158965ebf29f3f6c4f97d20456b8a
Yan Danilo Lima Morejon <yan.dl.morejon@gmail.com>
2025-05-28 10:29:43 -0300

    update

--- Aulas/Aula009/a009c_timer_proc.vhd
+++ Aulas/Aula009/a009c_timer_proc.vhd
@@ -5,8 +5,8 @@ entity a009c_timer_proc is
     generic(clockFreq   :   integer := 50e6); -- 10 Hz
     port(
         clk, nRst   :   in      std_logic;
-        s, m        :   inout   integer;
-        h           :   inout   integer
+        s, m        :   inout   integer range 0 to 60;
+        h           :   inout   integer range 0 to 60
     );
 end entity;
 
